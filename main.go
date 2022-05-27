e30d682fcfc425e2c320bef28940e606babc3ace
sanix-darker <s4nixd@gmail.com>
2022-05-27 11:04:17 +0200

    feat: setted a radConf function for the yaml file

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
 )
 
+type StorageUnit struct {
+	Dir         string `yaml:"directory"`
+	Url         string `yaml:"url"`
+	RefreshRate string `yaml:"refresh-rate"`
+}
+
+type Config struct {
+	Storage []StorageUnit `yaml:"storage"`
+}
+
+func readConf() {
+	filename, _ := filepath.Abs("./conf.yml")
+	yamlFile, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Value: %#v\n", config.Storage)
+}
+
 func main() {
+	// readConf()
 	fs := http.FileServer(http.Dir("./shared"))
 	http.Handle("/", fs)
 
